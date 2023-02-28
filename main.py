d0d27f612c343012ab2248403bb9e47bf80b86b4
eastyell <eastyell@163.com>
2023-02-28 20:01:32 +0800

    增加新内容

--- main.py
+++ main.py
@@ -40,3 +40,18 @@ if __name__ == '__main__':
     # t01.start()
     data = {i: i**2 for i in range(1, 9)}
     print(data)
+
+    s = 'sgjqsafgxvyiomsadf'
+    s = set(s)
+    l = list(s)
+    l.sort()
+    res = ''.join(l)
+    print(res)
+
+    # dict = {'name': 80, 'age': 18, 'city': 78, 'tel': 123}
+    # list = sorted(dict.items(), key=lambda i: i[1], reverse=False)
+    # print('sorted根据字典建排序：', list)
+
+    a = ['苏州', '中国', '哈哈', '', '日本', '', '英国']
+    res = list(map(lambda x: '无数据' if x == '' else x, a))
+    print(res)
