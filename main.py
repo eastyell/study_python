89eb6155798bdd234b388ab6ef1fd3bb5608004b
eastyell <eastyell@163.com>
2023-09-04 11:20:07 +0800

    更新

--- main.py
+++ main.py
@@ -45,48 +45,81 @@ if __name__ == '__main__':
 # 去除重复字符，并排序 升序
 # sorted（）是python的内置函数，可以排序的对象有列表、元组、字典、字符串。sorted不改变原对象的值，排序后的对象类型全部为list。
 # sort是列表类的方法，只对列表有用。不返回值，直接在列表上操作，会改变原对象的值。
-    s = 'sgjqsafgxvyiomsadf'
-    print('3，old str:', s)
-    # 方法1
-    res = set(s)
-    res = sorted(res)
-    res = ''.join(res)
-    print('去除重复字符并排序:', res)
-    # 方法2
-    s = set(s)
-    x = list(s)
-    x.sort()
-    res = ''.join(x)
-    print('去除重复字符并排序:', res)
+s = 'sgjqsafgxvyiomsadf'
+print('3，old str:', s)
+# 方法1
+res = set(s)
+res = sorted(res)
+res = ''.join(res)
+print('去除重复字符并排序:', res)
+# 方法2
+s = set(s)
+x = list(s)
+x.sort()
+res = ''.join(x)
+print('去除重复字符并排序:', res)
 
-    # 根据字典建排序
-    dict = {'chinese': 80, 'maths': 88, 'english': 78, 'history': 92}
-    print('4，old dict:', dict)
-    lists = sorted(dict.items(), key=lambda i: i[1], reverse=False)
-    print('sorted根据成绩排序：', lists)
+# 根据字典建排序
+dict = {'chinese': 80, 'maths': 88, 'english': 78, 'history': 92}
+print('4，old dict:', dict)
+lists = sorted(dict.items(), key=lambda i: i[1], reverse=False)
+print('sorted根据成绩排序：', lists)
 
-    # map函数
-    area = ['苏州', '中国', '哈哈', '', '日本', '', '英国']
-    print('5，原始数据：', area)
-    res = list(map(lambda x: '无数据' if x == '' else x, area))
-    print('填充数据：', res)
-
-    #  装饰器
-    def my_decorator(func):
-        def wrapper(arg):  # 传递参数
-            print("Something is happening before the function is called.")
-            func(arg)
-            print("Something is happening after the function is called.")
-
-        return wrapper
-    print('6，装饰器')
+# map函数
+area = ['苏州', '中国', '哈哈', '', '日本', '', '英国']
+print('5，原始数据：', area)
+res = list(map(lambda x: '无数据' if x == '' else x, area))
+print('填充数据：', res)
 
 
-    @my_decorator
-    def say_whee(arg):
-        print(arg + "Whee!")
+#  装饰器
+def my_decorator(func):
+    def wrapper(arg):  # 传递参数
+        print("Something is happening before the function is called.")
+        func(arg)
+        print("Something is happening after the function is called.")
+
+    return wrapper
+
+
+print('6，装饰器')
+@my_decorator
+def say_whee(arg):
+    print(arg + "Whee!")
 
 say_whee('hello ')
+
+
+# 类装饰器
+class MyDecorator(object):
+    def __init__(self, func):
+        self.__func = func
+
+    # 实现__call__方法，让对象变成可调用的对象，
+    # 可调用的对象能够像函数一样被使用。
+    def __call__(self, *args, **kwargs):
+        # 对已有参数进行封装
+        print('--正在进行类装饰-----')
+        self.__func()
+
+
+@MyDecorator
+def show():
+    print("hello")
+
+
+# 指向MyDecorator类创建实例对象--> show()==> 对象（）
+show()
+
+# 闭包
+def func_out(num1):
+    def func_inner(num2):
+        result = num1 + num2
+        print('结果是：', result)
+    return func_inner
+
+
+func_out(10)(10)
 
 # 三元函数
 # num = input('please input a num:')
